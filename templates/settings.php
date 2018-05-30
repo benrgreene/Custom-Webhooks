ab96a49908ac1bb23bd012c03802360571623a6c
Ben Greene <benrgreene@gmail.com>
2018-05-29 23:19:42 -0700

    chore: add available hooks to settings
    Added a list of all available plugins to the settings page
    as a reference to users

--- templates/settings.php
+++ templates/settings.php
@@ -1,45 +1,70 @@
 <div class="wrap">
     <h1>Webhooks</h1>
     <hr/>
-    <form method="POST" id="js-webhook-form" action="" enctype="multipart/form-data">
-        <?php settings_fields( self::SETTINGS_GROUP ); ?>
-        <h2>Settings</h2>
+    <div class="flex-container">
+        <div>
+            <form method="POST" id="js-webhook-form" action="" enctype="multipart/form-data">
+                <?php settings_fields( self::SETTINGS_GROUP ); ?>
+                <h2>Settings</h2>
 
-        <table id="js-webhook-table"><tbody>
-            <tr class="webhook-auth">
-                <td>Authentication:</td>
-                <?php if( current_user_can( 'activate_plugins' ) ): ?>
-                    <td><input name="brg-webhook-auth" id="brg-webhook-auth" value="<?php echo get_option('brg-webhook-auth'); ?>" /></td>
-                    <td><button id="generate-auth">Generate Authentication Key</button></td>
-                <?php else: ?>
-                    <td><b><?php echo get_option('brg-webhook-auth'); ?></b></td>
-                <?php endif; ?>
-            </tr>            
-        <?php
-            $table_manager = BRG_Webhook_Table_Manager::get_instance();
-            $webhooks = $table_manager->get_user_webhooks();
-            $webhooks = is_array( $webhooks ) ? $webhooks : array();
+                <table id="js-webhook-table"><tbody>
+                    <tr class="webhook-auth">
+                        <td>Authentication:</td>
+                        <?php if( current_user_can( 'activate_plugins' ) ): ?>
+                            <td><input name="brg-webhook-auth" id="brg-webhook-auth" value="<?php echo get_option('brg-webhook-auth'); ?>" /></td>
+                            <td><button id="generate-auth">Generate Authentication Key</button></td>
+                        <?php else: ?>
+                            <td><b><?php echo get_option('brg-webhook-auth'); ?></b></td>
+                        <?php endif; ?>
+                    </tr>            
+                <?php
+                    $table_manager = BRG_Webhook_Table_Manager::get_instance();
+                    $webhooks = $table_manager->get_user_webhooks();
+                    $webhooks = is_array( $webhooks ) ? $webhooks : array();
 
-            foreach( $webhooks as $index => $webhook ) { ?>
-                <tr class="new-webhook webhook-<?php echo $index; ?>">
-                    <td>Action</td>
-                    <td><input class="webhook-action " value="<?php echo $webhook['action']; ?>" /></td>
-                </tr>
-                <tr class="webhook-<?php echo $index; ?>">
-                    <td>Endpoint</td>
-                    <td><input class="webhook-endpoint" value="<?php echo $webhook['endpoint']; ?>" /></td>
-                </tr>
-                <tr class="webhook-<?php echo $index; ?>">
-                    <td class="remove-endpoint" data-index="<?php echo $index; ?>" >Remove Endpoint</td>
-                </tr>
-            <?php }
-        ?>
-        </tbody></table>
-        <input type="hidden" name="brg-webhooks" id="brg-webhooks" />
-    </form>
-    <div class="controls">
-        <button id="add-webhook">Add Row</button>
-        <button id="submit-webhooks">Submit</button>
+                    foreach( $webhooks as $index => $webhook ) { ?>
+                        <tr class="new-webhook webhook-<?php echo $index; ?>">
+                            <td>Action</td>
+                            <td><input class="webhook-action " value="<?php echo $webhook['action']; ?>" /></td>
+                        </tr>
+                        <tr class="webhook-<?php echo $index; ?>">
+                            <td>Endpoint</td>
+                            <td><input class="webhook-endpoint" value="<?php echo $webhook['endpoint']; ?>" /></td>
+                        </tr>
+                        <tr class="webhook-<?php echo $index; ?>">
+                            <td class="remove-endpoint" data-index="<?php echo $index; ?>" >Remove Endpoint</td>
+                        </tr>
+                    <?php }
+                ?>
+                </tbody></table>
+                <input type="hidden" name="brg-webhooks" id="brg-webhooks" />
+            </form>
+            <div class="controls">
+                <button id="add-webhook">Add Row</button>
+                <button id="submit-webhooks">Submit</button>
+            </div>
+        </div>
+        <div class="available-hooks">
+            <h2>Available Hooks</h2>
+            <p><b>Available Post Types</b></p>
+            <ul>
+                <?php $post_types = get_post_types(); ?>
+                <?php foreach( $post_types as $pt ): ?>
+                    <li><?php echo $pt; ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <p><b>Available Taxonomies</b></p>
+            <ul>
+                <?php $taxonomies = get_taxonomies(); ?>
+                <?php foreach( $taxonomies as $tax ): ?>
+                    <li><?php echo $tax; ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <p><b>Extras</b></p>
+            <ul>
+                <li>user_register</li>
+            </ul>
+        </div>
     </div>
 </div>
 <script type="text/javascript">
@@ -99,5 +124,17 @@
     }
     .remove-endpoint {
         color: red;
+    }
+    ul {
+        padding-left: 20px;
+    }
+    @media(min-width: 700px) {
+        .flex-container {
+            display: flex;
+            justify-content: space-between;
+        }
+        .available-hooks {
+            flex-basis: 40%;
+        }
     }
 </style>
